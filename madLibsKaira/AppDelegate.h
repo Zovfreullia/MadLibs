400f16621017af8c6e6902c0d2f29a9e4c0519ec
Fatima <fzf.villanueva@gmail.com>
2015-06-07 14:32:14 -0400

    initial state

---
+++ madLibsKaira/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  madLibsKaira
+//
+//  Created by Fatima Zenine Villanueva on 6/7/15.
+//  Copyright (c) 2015 apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ madLibsKaira/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  madLibsKaira
+//
+//  Created by Fatima Zenine Villanueva on 6/7/15.
+//  Copyright (c) 2015 apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
